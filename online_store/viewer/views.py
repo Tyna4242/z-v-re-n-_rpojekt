3660ad828f3469d8c0c22f0724a63d12ad116d6a
Tyna4242 <stribrnkri@natur.cuni.cz>
2024-09-15 10:53:42 +0200

    main template funguje

--- online_store/viewer/views.py
+++ online_store/viewer/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
 
-def MainPageView(TemplateView):
+class MainPageView(TemplateView):
     template_name = "main.html"
     extra_context = {}
+
