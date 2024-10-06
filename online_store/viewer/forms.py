7f23ce893523b658f0520102d10419c6048acd56
Tyna4242 <stribrnkri@natur.cuni.cz>
2024-10-06 08:57:22 +0200

    validace-title min 3 znaky

--- online_store/viewer/forms.py
+++ online_store/viewer/forms.py
@@ -1,7 +1,32 @@
 from django.forms import ModelForm
 from viewer.models import Category, Product
+from django import forms
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if len(title) < 3:
+            raise forms.ValidationError('Název produktu musí mít alespoň 3 znaky.')
+        return title
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
