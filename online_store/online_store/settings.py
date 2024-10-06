c1bf7fe377eb0505c7afdced407758664186c6c0
Tyna4242 <stribrnkri@natur.cuni.cz>
2024-10-06 07:20:22 +0200

    log in log out

--- online_store/online_store/settings.py
+++ online_store/online_store/settings.py
@@ -124,3 +124,6 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'potraviny'
+LOGOUT_REDIRECT_URL = 'potraviny'
--- online_store/viewer/urls.py
+++ online_store/viewer/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import MainPageView, BasePageView, PotravinyView, ProductCreateView, ProductUpdateView, ProductDeleteView, IndexView
-
+from django.contrib.auth.views import LogoutView, LoginView
 
 urlpatterns = [
     path('', MainPageView.as_view(), name='main-view'),
@@ -10,4 +10,6 @@ urlpatterns = [
     path('potraviny/update/<int:pk>/', ProductUpdateView.as_view(), name='potraviny-update-view'),
     path('potraviny/delete/<int:pk>/', ProductDeleteView.as_view(), name='potraviny-delete-view'),
     path('index/', IndexView.as_view(), name='index-view'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
